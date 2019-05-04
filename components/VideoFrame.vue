0e4d62ae531f2b91b3b6aed6d7cf74a3c9b92533
cheshirehat <cheshireinhatter@gmail.com>
2019-05-05 02:59:13 +0900

    とりあえず笹木:tanabata_tree:の動画でフレームのコンポーネント作成

---
+++ components/VideoFrame.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="youtube">
+    <iframe type="text/html" width="600" height="600"
+    src="http://www.youtube.com/embed/HZleluBJrjA?autoplay=1&origin=http://example.com"
+    frameborder="0"/>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.youtube {
+  position: relative;
+  width: 80%;
+  padding-top: 56.25%;
+}
+.youtube iframe {
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 100%;
+  height: 80%;
+}
+</style>
