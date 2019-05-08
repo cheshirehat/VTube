223c5886d9bf8a8766e49dad86853618feb4d70e
cheshirehat <cheshireinhatter@gmail.com>
2019-05-08 23:38:26 +0900

    YouTube APIからそれぞれ情報を取ってこれるようにした

---
+++ store/index.js
@@ -0,0 +1,34 @@
+import axios from 'axios'
+
+export const state = () => ({
+  vTuber: {},
+  channel: {},
+  movie: {}
+})
+
+export const getters = {
+
+}
+
+export const mutations = {
+  setVTuberData(state, value) {
+    state.vTuber = value
+  },
+  setChannel(state, value) {
+    state.channel = value
+  },
+  setMovie(state, value) {
+    state.movie = value
+  }
+}
+
+export const actions = {
+  async getVTuberData({commit, state}) {
+    const res = await axios.get(`https://www.googleapis.com/youtube/v3/channels?part=snippet&id=${ state.vTuber.channelId }&key=${ process.env.YOUTUBE_API_KEY }`)
+    commit('setChannel', res.data.items[0].snippet)
+  },
+  async getVTuberMovie({commit, state}) {
+    const res = await axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${ state.vTuber.channelId }&maxResults=50&order=date&key=${ process.env.YOUTUBE_API_KEY }`)
+    commit('setMovie', res)
+  }
+}
